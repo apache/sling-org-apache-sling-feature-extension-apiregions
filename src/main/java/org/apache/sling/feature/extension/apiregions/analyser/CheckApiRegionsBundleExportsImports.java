1f4f8f1904f70fad6d3a8ee4734d46b07bce84f7
Karl Pauls <kpauls@adobe.com>
2020-01-20 13:54:30 +0100

    SLING-8970 - adding test & fixing logic to consider inherited exports in region checks

--- src/main/java/org/apache/sling/feature/extension/apiregions/analyser/CheckApiRegionsBundleExportsImports.java
+++ src/main/java/org/apache/sling/feature/extension/apiregions/analyser/CheckApiRegionsBundleExportsImports.java
@@ -39,6 +39,7 @@ import org.apache.sling.feature.Extensions;
 import org.apache.sling.feature.Feature;
 import org.apache.sling.feature.analyser.task.AnalyserTask;
 import org.apache.sling.feature.analyser.task.AnalyserTaskContext;
+import org.apache.sling.feature.extension.apiregions.api.ApiExport;
 import org.apache.sling.feature.extension.apiregions.api.ApiRegion;
 import org.apache.sling.feature.extension.apiregions.api.ApiRegions;
 import org.apache.sling.feature.scanner.BundleDescriptor;
@@ -207,6 +208,18 @@ public class CheckApiRegionsBundleExportsImports implements AnalyserTask {
                                     Set<String> imRegions =
                                             getBundleRegions(info, apiRegions, ignoreAPIRegions);
 
+                                    // Check exports of the importing regions to consider inherited exports
+                                    for (String imRegion : imRegions)
+                                    {
+                                        ApiRegion region = apiRegions.getRegionByName(imRegion);
+                                        for (ApiExport export : region.listAllExports())
+                                        {
+                                            if (export.getName().equals(pck.getName()))
+                                            {
+                                                exRegions.add(imRegion);
+                                            }
+                                        }
+                                    }
                                     // Record the exporting and importing regions for diagnostics
                                     exportingRegions.addAll(exRegions);
                                     importingRegions.addAll(imRegions);
